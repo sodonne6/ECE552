481422a923b4eaef81949ad8940fb56a1cf272c9
pbdavis2 <pbdavis2@wisc.edu>
2025-03-01 15:40:16 -0600

    4bit for CLA

---
+++ phase1_files/add_sub_4.v
@@ -0,0 +1,25 @@
+module addsub_4bit(
+    input [3:0] A, B,
+    input Cin,
+    output [3:0] Sum,
+    output Cout, PG, GG
+);
+    wire [3:0] P, G;
+    wire [3:1] C;
+    
+    assign P = A ^ B; // propagate
+    assign G = A & B; // generate
+    
+    assign C[1] = G[0] | (P[0] & Cin);
+    assign C[2] = G[1] | (P[1] & C[1]);
+    assign C[3] = G[2] | (P[2] & C[2]);
+    assign Cout = G[3] | (P[3] & C[3]);
+    
+    assign Sum[0] = P[0] ^ Cin;
+    assign Sum[1] = P[1] ^ C[1];
+    assign Sum[2] = P[2] ^ C[2];
+    assign Sum[3] = P[3] ^ C[3];
+    
+    assign PG = &P; // group propagate
+    assign GG = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]); // group generate 
+endmodule
