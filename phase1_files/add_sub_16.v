5011c28ef65c97c3ab420974b78dc12d21544e98
pbdavis2 <pbdavis2@wisc.edu>
2025-03-01 15:38:58 -0600

    Update add_sub_16.v

--- phase1_files/add_sub_16.v
+++ phase1_files/add_sub_16.v
@@ -11,10 +11,10 @@ module add_sub_16(
     assign B_xor = B ^ {16{Sub}}; //negate if subtraction
     assign Cin = Sub; // add 1 if negated
 
-    CLA_4bit cla0(A[3:0], B_xor[3:0], Cin, Sum[3:0], C[0], PG[0], GG[0]);
-    CLA_4bit cla1(A[7:4], B_xor[7:4], C[0], Sum[7:4], C[1], PG[1], GG[1]);
-    CLA_4bit cla2(A[11:8], B_xor[11:8], C[1], Sum[11:8], C[2], PG[2], GG[2]);
-    CLA_4bit cla3(A[15:12], B_xor[15:12], C[2], Sum[15:12], C[3], PG[3], GG[3]);
+    add_sub_4 cla0(A[3:0], B_xor[3:0], Cin, Sum[3:0], C[0], PG[0], GG[0]);
+    add_sub_4 cla1(A[7:4], B_xor[7:4], C[0], Sum[7:4], C[1], PG[1], GG[1]);
+    add_sub_4 cla2(A[11:8], B_xor[11:8], C[1], Sum[11:8], C[2], PG[2], GG[2]);
+    add_sub_4 cla3(A[15:12], B_xor[15:12], C[2], Sum[15:12], C[3], PG[3], GG[3]);
 
     assign Ovfl = GG[3] | (PG[3] & C[3]);
 endmodule
