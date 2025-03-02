94a24e11a443347e0e7d29ebfc8a4f7bbcb0984d
sodonne6 <odonnellsd1@gmail.com>
2025-03-02 14:09:51 -0600

    Update PADDSB_16.v
    slight syntax error fixed

--- phase1_files/PADDSB_16.v
+++ phase1_files/PADDSB_16.v
@@ -18,8 +18,8 @@ module PSA_16bit (Sum, Error, A, B);
 
 	add_sub_4 s0 (.A(A[3:0]), .B(B[3:0]), .Cin(0), .Sum(sum0), .Ovfl(ovfl0), .Cout(), .PG(), .GG());
 	add_sub_4 s1 (.A(A[7:4]), .B(B[7:4]), .Cin(0), .Sum(sum1), .Ovfl(ovfl1), .Cout(), .PG(), .GG());
-	add_sub_4 s0 (.A(A[11:8]), .B(B[11:8]), .Cin(0), .Sum(sum0), .Ovfl(ovfl2), .Cout(), .PG(), .GG());
-	add_sub_4 s0 (.A(A[15:12]), .B(B[15:12]), .Cin(0), .Sum(sum0), .Ovfl(ovfl3), .Cout(), .PG(), .GG());
+	add_sub_4 s2 (.A(A[11:8]), .B(B[11:8]), .Cin(0), .Sum(sum0), .Ovfl(ovfl2), .Cout(), .PG(), .GG());
+	add_sub_4 s3 (.A(A[15:12]), .B(B[15:12]), .Cin(0), .Sum(sum0), .Ovfl(ovfl3), .Cout(), .PG(), .GG());
 
 	//if any overflow flag is high error flagged
 	assign Error = ovf3 | ovf2 | ovf1 | ovf0;
