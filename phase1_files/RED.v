f98e5d909ed95d6b106c0d3533a68c6a7ccf2609
pbdavis2 <pbdavis2@wisc.edu>
2025-03-03 21:41:31 -0600

    checked w/ TB

--- phase1_files/RED.v
+++ phase1_files/RED.v
@@ -1,42 +1,75 @@
-module red(
-    	input [15:0] A, B,
-    	output [15:0] Sum
+module RED (
+  input [15:0] a,  
+  input [15:0] b,  
+  output [15:0] sum 
 );
-    	wire [4:0] sum_ae, sum_bf, sum_cg, sum_dh;
-    	wire [4:0] sum_ae_bf, sum_cg_dh;
-	wire [1:0] sum_ae_bf_out, sum_cg_dh_out;
-    	wire [5:0] final_sum;
-	wire [2:0] ae_bf_cg_dh_out;
-	wire [4:0] final_MSB;
-	
-    	//level 1 - add each 4 bits and log the carry out bits as the MSB's
-    	add_sub_4 adder1(.A(A[15:12]), .B(B[15:12]), .Cin(1'b0), .Sum(sum_ae[3:0]), .Cout(sum_ae[4]), .PG(), .GG(), .Ovfl());
-    	add_sub_4 adder2(.A(A[11:8]),  .B(B[11:8]),  .Cin(1'b0), .Sum(sum_bf[3:0]), .Cout(sum_bf[4]), .PG(), .GG(), .Ovfl());
-    	add_sub_4 adder3(.A(A[7:4]),   .B(B[7:4]),   .Cin(1'b0), .Sum(sum_cg[3:0]), .Cout(sum_cg[4]), .PG(), .GG(), .Ovfl());
-    	add_sub_4 adder4(.A(A[3:0]),   .B(B[3:0]),   .Cin(1'b0), .Sum(sum_dh[3:0]), .Cout(sum_dh[4]), .PG(), .GG(), .Ovfl());
-	
-    	//level 2 - add 4 bit parts first
-    	add_sub_4 adder5(.A(sum_ae[3:0]), .B(sum_bf[3:0]), .Cin(1'b0), .Sum(sum_ae_bf[3:0]), .Cout(sum_ae_bf[4]), .PG(), .GG(), .Ovfl());
-    	add_sub_4 adder6(.A(sum_cg[3:0]), .B(sum_dh[3:0]), .Cin(1'b0), .Sum(sum_cg_dh[3:0]), .Cout(sum_cg_dh[4]), .PG(), .GG(), .Ovfl());
-	//then add carry out bits - append 0's to inputs to make 4 bit
-	//the output will be 2 bits at most
-	//no cout needed
-	add_sub_4 adder7(.A({3'b0,sum_ae[4]}), .B({3'b0,sum_bf[4]}), .Cin(1'b0), .Sum(sum_ae_bf_out), .Cout(), .PG(), .GG(), .Ovfl());	
-    	add_sub_4 adder8(.A({3'b0,sum_cg[4]}), .B({3'b0,sum_dh[4]}), .Cin(1'b0), .Sum(sum_cg_dh_out), .Cout(), .PG(), .GG(), .Ovfl());
+    wire [3:0] sum0, sum1, sum2, sum3, sum01, sum23, total_sum, sum01c, sum23c, total_sumc;
+    wire [3:0] c00, c01, c02, c03;
+    wire [3:0] c0;
+    wire [1:0] c1;
+    wire c2;
+    wire ovfl0, ovfl1, ovfl2, ovfl3;
+    wire ovfl01, ovfl23, ovfl_final;
+    
+    // Level 1
+    add_sub_4 ADD_0 (
+        .A(a[15:12]), .B(b[15:12]), .Cin(1'b0),
+        .Sum(sum0), .Cout(c0[0]), .Ovfl(ovfl0), .PG(), .GG()
+    );
+    
+    add_sub_4 ADD_1 (
+        .A(a[11:8]), .B(b[11:8]), .Cin(1'b0),
+        .Sum(sum1), .Cout(c0[1]), .Ovfl(ovfl1), .PG(), .GG()
+    );
+    
+    add_sub_4 ADD_2 (
+        .A(a[7:4]), .B(b[7:4]), .Cin(1'b0),
+        .Sum(sum2), .Cout(c0[2]), .Ovfl(ovfl2), .PG(), .GG()
+    );
+    
+    add_sub_4 ADD_3 (
+        .A(a[3:0]), .B(b[3:0]), .Cin(1'b0),
+        .Sum(sum3), .Cout(c0[3]), .Ovfl(ovfl3), .PG(), .GG()
+    );
+    
+    // 1 -> 4-bit
+    assign c00 = {4{(ovfl0?c0[0]:sum0[3])}};
+    assign c01 = {4{(ovfl1?c0[1]:sum1[3])}};
+    assign c02 = {4{(ovfl2?c0[2]:sum2[3])}};
+    assign c03 = {4{(ovfl3?c0[3]:sum3[3])}};
 
+    // Level 2
+    add_sub_4 ADD_01 (
+        .A(sum0), .B(sum1), .Cin(1'b0),
+        .Sum(sum01), .Cout(c1[0]), .Ovfl(ovfl01), .PG(), .GG()
+    );
 
-	//LEVEL 3 
-	add_sub_4 adder9(.A(sum_ae_bf[3:0]), .B(sum_cg_dh[3:0]), .Cin(1'b0), .Sum(final_sum[3:0]), .Cout(final_sum[4]), .PG(), .GG(), .Ovfl());
-	//add carry out from last level together
-	//max 3 bits 
-	add_sub_4 adder10(.A({2'b0,sum_ae_bf_out}), .B({2'b0,sum_cg_dh_out}), .Cin(1'b0), .Sum(ae_bf_cg_dh_out), .Cout(), .PG(), .GG(), .Ovfl());
-	//last step is adder for cout from adder9 and sum of adder10
-	add_sub_4 adder11(.A({1'b0,ae_bf_cg_dh_out}), .B({3'b0,final_sum[4]}), .Cin(1'b0), .Sum(final_MSB), .Cout(), .PG(), .GG(), .Ovfl());
+    add_sub_4 ADD_01C (
+        .A(c00), .B(c01), .Cin(c1[0]),
+        .Sum(sum01c), .Cout(), .Ovfl(), .PG(), .GG()
+    );
+    
+    add_sub_4 ADD_23 (
+        .A(sum2), .B(sum3), .Cin(1'b0),
+        .Sum(sum23), .Cout(c1[1]), .Ovfl(ovfl23), .PG(), .GG()
+    );
 
-	assign Sum = {{10{final_MSB[4]}}, final_MSB, final_sum[3:0]};
-	
+    add_sub_4 ADD_23C (
+        .A(c02), .B(c03), .Cin(c1[1]),
+        .Sum(sum23c), .Cout(), .Ovfl(), .PG(), .GG()
+    );
+    
+    // Level 3
+    add_sub_4 ADD_FINAL (
+        .A(sum01), .B(sum23), .Cin(1'b0),
+        .Sum(total_sum), .Cout(c2), .Ovfl(ovfl_final), .PG(), .GG()
+    );
 
-    	
-    	
-
+    add_sub_4 ADD_FINALC (
+        .A(sum01c), .B(sum23c), .Cin(c2),
+        .Sum(total_sumc), .Cout(), .Ovfl(), .PG(), .GG()
+    );
+    
+    // use total_sumc[2] for sign extension, as it is the MSB of the significant bits
+    assign sum = {{9{total_sumc[2]}}, total_sumc[2:0], total_sum};
 endmodule
